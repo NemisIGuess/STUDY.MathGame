43ada657d9c30e8fb8fdb105b80c286b80bcb529
NemisIGuess <ismaelpjgg@gmail.com>
2023-03-13 22:55:22 +0100

    Practiced the manipulation of text while learning about different data types.

--- STUDY.MathGame/Program.cs
+++ STUDY.MathGame/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy!");
+﻿int index = 1;
+string name = "Ismael";
+char initial = 'I';
+int year = 2023;
+decimal height = 1.88m;
+bool doWeLoveToCode = true;
+
+string myParagraph = $@"These are the most common data types: 
+{index++} - string, example: {name}
+{index++} - char, example: {initial}
+{index++} - integer, example: {year}
+{index++} - decimal, example: {height}
+{index++} - boolean, example: {doWeLoveToCode}";
+
+Console.WriteLine(myParagraph);
+
+Console.ReadLine();
