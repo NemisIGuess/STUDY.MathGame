b9b7fee70a1b34f832664aaa588ed1e7ae7fc2a0
NemisIGuess <ismaelpjgg@gmail.com>
2023-03-14 23:18:49 +0100

    Started work on the Math Game. - Added game selection menu.

--- STUDY.MathGame/Program.cs
+++ STUDY.MathGame/Program.cs
@@ -1,17 +1,47 @@
-﻿int index = 1;
-string name = "Ismael";
-char initial = 'I';
-int year = 2023;
-decimal height = 1.88m;
-bool doWeLoveToCode = true;
+﻿Console.WriteLine("Please type your name");
 
-string myParagraph = $@"These are the most common data types: 
-{index++} - string, example: {name}
-{index++} - char, example: {initial}
-{index++} - integer, example: {year}
-{index++} - decimal, example: {height}
-{index++} - boolean, example: {doWeLoveToCode}";
+string name = Console.ReadLine();
+DateTime date = DateTime.UtcNow;
 
-Console.WriteLine(myParagraph);
+Console.WriteLine("-------------------------------------------");
 
-Console.ReadLine();
+Console.WriteLine($"Hello {name}. It's {date.DayOfWeek}.\nThis is your math game. That's great that you're working on improving yourself.\n");
+Console.WriteLine($@"What game would you like to play today? Choose from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+
+Console.WriteLine("-------------------------------------------");
+
+string gameSelection = Console.ReadLine();
+
+if (gameSelection.Trim().ToLower() == "a")
+{
+    Console.WriteLine("Addition game selected.");
+}
+else if (gameSelection.Trim().ToLower().Trim().ToLower() == "s")
+{
+    Console.WriteLine("Subtraction game selected.");
+}
+else if (gameSelection.Trim().ToLower() == "m")
+{
+    Console.WriteLine("Multiplication game selected.");
+}
+else if (gameSelection.Trim().ToLower() == "d" )
+{
+    Console.WriteLine("Division game selected.");
+}
+else if (gameSelection.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye !");
+    System.Threading.Thread.Sleep(500);
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("Input not recognized, the program will now close.");
+    System.Threading.Thread.Sleep(1500);
+    Environment.Exit(1);
+}
