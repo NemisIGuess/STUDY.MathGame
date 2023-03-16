6130240683c8dcb524d953e2eaedcec8c483aa16
NemisIGuess <ismaelpjgg@gmail.com>
2023-03-16 22:57:34 +0100

    Continued work on Math Game · Created the game methods for all 4 games. · Cleaned up the code.

--- STUDY.MathGame/Program.cs
+++ STUDY.MathGame/Program.cs
@@ -1,17 +1,22 @@
-﻿Console.WriteLine("Please type your name");
+﻿using System.Threading.Tasks.Sources;
+using static System.Formats.Asn1.AsnWriter;
 
-string name = Console.ReadLine();
-DateTime date = DateTime.UtcNow;
+string name = GetName();
 
-Menu(name, date);
+Menu(name);
 
+string GetName()
+{
+    Console.WriteLine("Please type your name");
 
-#region Menu
-void Menu(string name, DateTime date)
+    string name = Console.ReadLine();
+    return name;
+}
+void Menu(string name)
 {
     Console.WriteLine("-------------------------------------------");
 
-    Console.WriteLine($"Hello {name}. It's {date.DayOfWeek}.\nThis is your math game. That's great that you're working on improving yourself.\n");
+    Console.WriteLine($"Hello {name} !.\nThis is your math game. That's great that you're working on improving yourself.\n");
     Console.WriteLine($@"What game would you like to play today? Choose from the options below:
 A - Addition
 S - Subtraction
@@ -26,16 +31,16 @@ Q - Quit the program");
     switch (gameSelection.Trim().ToLower())
     {
         case "a":
-            AdditionGame("Addition game selected.");
+            AdditionGame("Addition game.");
             break;
         case "s":
-            SubtractionGame("Subtraction game selected.");
+            SubtractionGame("Subtraction game.");
             break;
         case "m":
-            MultiplicationGame("Multiplication game selected.");
+            MultiplicationGame("Multiplication game.");
             break;
         case "d":
-            DivisionGame("Division game selected.");
+            DivisionGame("Division game.");
             break;
         case "q":
             Console.WriteLine("Goodbye !");
@@ -49,24 +54,160 @@ Q - Quit the program");
     }
 }
 
-#endregion
-
 #region GameMethods
 void AdditionGame(string messageToPrint)
 {
+    Console.Clear();
     Console.WriteLine(messageToPrint);
+
+    int score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Random randomGenerator = new Random();
+        firstNumber = randomGenerator.Next(1, 9);
+        secondNumber = randomGenerator.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} + {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber + secondNumber)
+        {
+            Console.WriteLine("Your answer was correct! Type any key for the next question.");
+            Console.ReadLine();
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect! Type any key for the next question.");
+            Console.ReadLine();
+        }
+
+        if (i == 4) Console.WriteLine($"Your score is {score}");
+    }
+
 }
 void SubtractionGame(string messageToPrint)
 {
+    Console.Clear();
     Console.WriteLine(messageToPrint);
+
+    int score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Random randomGenerator = new Random();
+        firstNumber = randomGenerator.Next(1, 9);
+        secondNumber = randomGenerator.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} - {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber - secondNumber)
+        {
+            Console.WriteLine("Your answer was correct! Type any key for the next question.");
+            Console.ReadLine();
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect! Type any key for the next question.");
+            Console.ReadLine();
+        }
+
+        if (i == 4) Console.WriteLine($"Your score is {score}");
+    }
 }
 void MultiplicationGame(string messageToPrint)
 {
+    Console.Clear();
     Console.WriteLine(messageToPrint);
+
+    int score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Random randomGenerator = new Random();
+        firstNumber = randomGenerator.Next(1, 9);
+        secondNumber = randomGenerator.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} x {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber * secondNumber)
+        {
+            Console.WriteLine("Your answer was correct! Type any key for the next question.");
+            Console.ReadLine();
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect! Type any key for the next question.");
+            Console.ReadLine();
+        }
+
+        if (i == 4) Console.WriteLine($"Your score is {score}");
+    }
 }
 void DivisionGame(string messageToPrint)
 {
+    Console.Clear();
     Console.WriteLine(messageToPrint);
+    int score = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        int[] divisonNumbers = DivisionNumberGenerator();
+        int firstNumber = divisonNumbers[0];
+        int secondNumber = divisonNumbers[1];
+
+        Console.WriteLine($"{firstNumber} / {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber / secondNumber)
+        {
+            Console.WriteLine("Your answer was correct! Type any key for the next question.");
+            Console.ReadLine();
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect! Type any key for the next question.");
+            Console.ReadLine();
+        }
+
+        if (i == 4) Console.WriteLine($"Your score is {score}");
+    }
+
+}
+
+int[] DivisionNumberGenerator()
+{
+    Random randomGenerator = new Random();
+    int firstNumber = randomGenerator.Next(1, 99);
+    int secondNumber = randomGenerator.Next(1, 99);
+
+    int[] result = new int[2];
+
+    while (firstNumber % secondNumber != 0)
+    {
+        firstNumber = randomGenerator.Next(1, 99);
+        secondNumber = randomGenerator.Next(1, 99);
+    }
+
+    result[0] = firstNumber;
+    result[1] = secondNumber;
+
+    return result;
 }
 
 #endregion
